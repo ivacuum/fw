adb417af8010cf19500a0f96d3ddcf31e276f58a
Sergey Pankov <vacuum@ivacuum.ru>
2013-02-11 00:25:22 +0400

    Отключение вывода ошибок в браузер

--- bootstrap.php
+++ bootstrap.php
@@ -17,6 +17,7 @@ define('FW_DIR', __DIR__ . '/');
 define('SITE_DIR', rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/');
 
 date_default_timezone_set('Europe/Moscow');
+ini_set('display_errors', false);
 error_reporting(E_ALL);
 mb_internal_encoding('utf-8');
 
