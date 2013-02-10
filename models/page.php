0648e57bcd2bdf07e261d495bf898fcb1585a889
Sergey Pankov <vacuum@ivacuum.ru>
2013-02-11 00:07:04 +0400

    Передача функции humn_size шаблонизатору

--- models/page.php
+++ models/page.php
@@ -87,6 +87,7 @@ class page
 	public function _set_user($user)
 	{
 		$this->user = $user;
+		$this->template->add_function('humn_size', [$this->user, 'humn_size']);
 		$this->template->add_function('lang', [$this->user, 'lang']);
 		$this->template->add_function('plural', [$this->user, 'plural']);
 		
