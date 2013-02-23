1a3053c44c6b07abf8fb1ff39598737002528ff8
Sergey Pankov <vacuum@ivacuum.ru>
2013-02-24 00:36:57 +0400

    Исправление в построении ссылок на динамические разделы

--- cache/service.php
+++ cache/service.php
@@ -210,6 +210,7 @@ class service
 					left_id ASC';
 			$this->db->query($sql);
 			$traversal = new traverse_handlers_urls();
+			$traversal->_set_config($this->config);
 			
 			while ($row = $this->db->fetchrow())
 			{
