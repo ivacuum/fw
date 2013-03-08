9d2bb4fa5bc4a51f6c06d9013d8a155f5675ee60
Sergey Pankov <vacuum@ivacuum.ru>
2013-03-08 15:39:25 +0400

    Подготовка к подключению Swift Mailer

---
+++ core/mailer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package fw
+* @copyright (c) 2013
+*/
+
+namespace fw\core;
+
+require("{$app['dir.lib']}/swiftmailer/{$app['version.swift']}/swift_init.php");
+
+/**
+* Почтовик
+*/
+class mailer
+{
+}
