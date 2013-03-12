abf5b70e973c9a5dea3f4bd3e67d1380b81e4f6d
Sergey Pankov <vacuum@ivacuum.ru>
2013-03-12 12:34:20 +0400

    Перенос метода cache->obtain_image_stats

--- cache/service.php
+++ cache/service.php
@@ -261,48 +261,6 @@ class service
 	}
 
 	/**
-	* Статистика по изображениям в галерее
-	*/
-	public function obtain_image_stats()
-	{
-		if (false === $stats = $this->driver->get('image_stats'))
-		{
-			$stats = [];
-
-			/* Количество изображений, загруженных за последние сутки */
-			$sql = '
-				SELECT
-					COUNT(*) as today_images
-				FROM
-					' . IMAGES_TABLE . '
-				WHERE
-					image_time >= UNIX_TIMESTAMP(CURRENT_DATE())';
-			$this->db->query($sql);
-			$row = $this->db->fetchrow();
-			$this->db->freeresult();
-			$stats += $row;
-
-			/* Общая статистика */
-			$sql = '
-				SELECT
-					COUNT(*) AS total_images,
-					SUM(image_size) AS total_size,
-					SUM(image_size * image_views) AS total_traffic,
-					SUM(image_views) AS total_views
-				FROM
-					' . IMAGES_TABLE;
-			$this->db->query($sql);
-			$row = $this->db->fetchrow();
-			$this->db->freeresult();
-			$stats += $row;
-
-			$this->driver->set('image_stats', $stats, 120);
-		}
-
-		return $stats;
-	}
-
-	/**
 	* Список доступных языков
 	*/
 	public function obtain_languages($force_reload = false)
