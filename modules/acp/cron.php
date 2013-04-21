3bc784515230447085b2c9983f0e2724c7c99848
Sergey Pankov <vacuum@ivacuum.ru>
2013-04-21 17:31:25 +0400

    Возможность просмотра списка задач по расписанию

---
+++ modules/acp/cron.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* @package fw
+* @copyright (c) 2013
+*/
+
+namespace fw\modules\acp;
+
+use app\models\page;
+
+class sessions extends page
+{
+	public function index()
+	{
+		$sql = 'SELECT * FROM site_cron WHERE site_id = ? ORDER BY run_order ASC';
+		$this->db->query($sql, [$this->site_id]);
+		$this->template->assign('entries', $this->db->fetchall());
+		$this->db->freeresult();
+	}
+	
+	public function add()
+	{
+	}
+	
+	public function delete()
+	{
+	}
+	
+	public function edit()
+	{
+	}
+}
