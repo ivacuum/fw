e0d399e52b2704a56ae3cf82c4b443bde9186761
Sergey Pankov <vacuum@ivacuum.ru>
2013-04-27 11:44:58 +0400

    Уменьшение количества отладочной информации в письме из терминала

--- core/errorhandler.php
+++ core/errorhandler.php
@@ -263,6 +263,12 @@ class errorhandler
 			$call_stack = str_replace(static::$document_root, '', ob_get_clean());
 		}
 		
+		if (PHP_SAPI == 'cli')
+		{
+			mail(static::$options["email.{$email}"], $title, sprintf("[%s]\n%s\n%s\$_SERVER => %s", strftime('%c'), $text, $call_stack, print_r($_SERVER, true)), sprintf("From: fw@%s\r\n", gethostname()));
+			return;
+		}
+		
 		mail(static::$options["email.{$email}"], $title, sprintf("[%s]\n%s\n%s\$app['user']->data => %s\n\$_SERVER => %s\n\$_REQUEST => %s", strftime('%c'), $text, $call_stack, !empty($app['user']) ? print_r($app['user']->data, true) : '', print_r($_SERVER, true), print_r($_REQUEST, true)), sprintf("From: fw@%s\r\n", gethostname()));
 	}
 
