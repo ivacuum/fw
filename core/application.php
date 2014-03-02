75ba8aa5613c0fd9ff2bf49950678a9c828ce4e2
Sergey Pankov <vacuum@ivacuum.ru>
2014-03-02 21:57:43 +0400

    Рефакторинг контейнера приложения

--- core/application.php
+++ core/application.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @package fw
-* @copyright (c) 2013
+* @copyright (c) 2014
 */
 
 namespace fw\core;
@@ -17,6 +17,7 @@ use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\NativeMailerHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Processor\WebProcessor;
+use SplObjectStorage;
 use fw\captcha\service as captcha_service;
 use fw\captcha\validator as captcha_validator;
 use fw\cron\manager as cron_manager;
@@ -31,79 +32,88 @@ class application implements ArrayAccess
 {
 	const VERSION = '1.6.2';
 	
-	private $values;
+	private $values = [];
+	private $factories;
+	private $protected;
+	private $frozen = [];
+	private $raw = [];
+	private $keys = [];
 	
 	function __construct(array $values = [])
 	{
-		$this->values = $values;
+		$this->factories = new SplObjectStorage();
+		$this->protected = new SplObjectStorage();
+		
+		foreach ($values as $key => $value) {
+			$this->offsetSet($key, $value);
+		}
+		
 		$app = $this;
 		
-		$this['profiler'] = $this->share(function() use ($app) {
+		$this['profiler'] = function() use ($app) {
 			return new profiler(START_TIME, $app['profiler.options']);
-		});
+		};
 		
-		$this['autoloader'] = $this->share(function() use ($app) {
+		$this['autoloader'] = function() use ($app) {
 			require FW_DIR . 'core/autoloader.php';
 			
 			return (new autoloader())
 				->register_namespaces($app['autoloader.namespaces'])
 				->register_pears($app['autoloader.pears'])
 				->register();
-		});
+		};
 		
-		$this['template'] = $this->share(function() use ($app) {
+		$this['template'] = function() use ($app) {
 			define('SMARTY_DIR', "{$app['dir.lib']}/smarty/{$app['version.smarty']}/Smarty/");
 			require SMARTY_DIR . 'Smarty.class.php';
 
 			return new smarty([$app['dir.templates.app'], $app['dir.templates.fw']], $app['dir.templates.cache']);
-		});
+		};
 		
-		$this['request'] = $this->share(function() use ($app) {
+		$this['request'] = function() use ($app) {
 			return new request($app['request.options']);
-		});
+		};
 		
-		$this['db'] = $this->share(function() use ($app) {
+		$this['db'] = function() use ($app) {
 			return new db_mysqli($app['cache.driver'], $app['profiler'], $app['db.options']);
-		});
+		};
 		
-		$this['cache.driver'] = $this->share(function() use ($app) {
+		$this['cache.driver'] = function() use ($app) {
 			$class = "\\fw\\cache\\driver\\{$app['cache.driver.options']['type']}";
 			return new $class($app['cache.driver.options']);
-		});
+		};
 		
-		$this['cache'] = $this->share(function() use ($app) {
-			if (file_exists("{$app['dir.app']}/cache/service.php"))
-			{
+		$this['cache'] = function() use ($app) {
+			if (file_exists("{$app['dir.app']}/cache/service.php")) {
 				return new \app\cache\service($app['db'], $app['cache.driver']);
 			}
 			
 			return new \fw\cache\service($app['db'], $app['cache.driver']);
-		});
+		};
 
-		$this['user'] = $this->share(function() use ($app) {
+		$this['user'] = function() use ($app) {
 			return (new user($app['cache'], $app['config'], $app['db'], $app['request'], $app['session.options'], $app['site_info']['id'], $app['urls']['signin']))
 				->setup();
-		});
+		};
 		
-		$this['auth'] = $this->share(function() use ($app) {
+		$this['auth'] = function() use ($app) {
 			return (new auth($app['cache'], $app['db'], $app['user']))
 				->init($app['user']->data);
-		});
+		};
 
 		/* Настройки сайта и движка */
-		$this['config'] = $this->share(function() use ($app) {
+		$this['config'] = function() use ($app) {
 			return new config_db($app['cache'], $app['db'], $app['site_info']);
-		});
+		};
 
-		$this['router'] = $this->share(function() use ($app) {
+		$this['router'] = function() use ($app) {
 			return (new router($app['router.options']))
 				->_set_app($app);
-		});
+		};
 
 		/* Информация об обслуживаемом сайте */
-		$this['site_info'] = $this->share(function() use ($app) {
-			if (false === $site_info = $app['cache']->get_site_info_by_url($app['request']->hostname, $app['request']->url))
-			{
+		$this['site_info'] = function() use ($app) {
+			if (false === $site_info = $app['cache']->get_site_info_by_url($app['request']->hostname, $app['request']->url)) {
 				trigger_error('Сайт не найден', E_USER_ERROR);
 			}
 			
@@ -113,35 +123,35 @@ class application implements ArrayAccess
 			setlocale(LC_ALL, $site_info['locale']);
 			
 			return $site_info;
-		});
+		};
 		
-		$this['captcha'] = $this->share(function() use ($app) {
+		$this['captcha'] = function() use ($app) {
 			$class = "\\fw\\captcha\\driver\\{$app['captcha.type']}";
 
 			return new captcha_service($app['config'], $app['db'], $app['request'], $app['user'], new $class($app['dir.fonts'], $app['captcha.fonts']));
-		});
+		};
 		
-		$this['captcha_validator'] = $this->share(function() use ($app) {
+		$this['captcha_validator'] = function() use ($app) {
 			return new captcha_validator($app['config'], $app['db'], $app['request'], $app['user']);
-		});
+		};
 		
-		$this['cron'] = $this->share(function() use ($app) {
+		$this['cron'] = function() use ($app) {
 			return (new cron_manager($app['dir.logs'], $app['file.cron.allowed'], $app['file.cron.running']))
 				->_set_app($app);
-		});
+		};
 		
-		$this['form'] = $this->share(function() use ($app) {
+		$this['form'] = function() use ($app) {
 			return new form($app['config'], $app['db'], $app['request'], $app['template']);
-		});
+		};
 		
-		$this['http_client'] = $this->share(function() use ($app) {
+		$this['http_client'] = function() use ($app) {
 			$client = new Client();
 			$client->addSubscriber(new LogPlugin(new MonologLogAdapter($app['logger']), $app['logger.options']['guzzle.format']));
 			
 			return $client;
-		});
+		};
 		
-		$this['logger'] = $this->share(function() use ($app) {
+		$this['logger'] = function() use ($app) {
 			$logger = new Logger($app['site_info']['domain']);
 			$email  = $app['errorhandler.options']['email.error'];
 			
@@ -159,8 +169,7 @@ class application implements ArrayAccess
 			/* info и выше */
 			$logger->pushHandler(new DBHandler($app['db'], $app['request']));
 			
-			// if ($email)
-			// {
+			// if ($email) {
 			// 	/* warn и выше */
 			// 	$logger->pushHandler(new NativeMailerHandler($email, $app['request']->server_name, 'fw@' . gethostname()));
 			// }
@@ -174,30 +183,27 @@ class application implements ArrayAccess
 			});
 			
 			return $logger;
-		});
+		};
 		
-		$this['mailer'] = $this->share(function() use ($app) {
+		$this['mailer'] = function() use ($app) {
 			require "{$app['dir.lib']}/swiftmailer/{$app['version.swift']}/swift_init.php";
 
 			return new mailer($app['config'], $app['template']);
-		});
+		};
 		
-		$this['sphinx'] = $this->share(function() use ($app) {
+		$this['sphinx'] = function() use ($app) {
 			return new db_sphinx($app['cache.driver'], $app['profiler'], $app['sphinx.options']);
-		});
+		};
 		
-		foreach ($this['include.files'] as $file)
-		{
+		foreach ($this['include.files'] as $file) {
 			require $file;
 		}
 		
-		if ($this['autoloader.options']['enabled'])
-		{
+		if ($this['autoloader.options']['enabled']) {
 			$this['autoloader'];
 		}
 		
-		if ($this['errorhandler.options']['enabled'])
-		{
+		if ($this['errorhandler.options']['enabled']) {
 			errorhandler::register($this['errorhandler.options']);
 		}
 	}
@@ -207,73 +213,83 @@ class application implements ArrayAccess
 	*
 	* Полезно, когда необходимо расширить объект, не инициализируя его
 	*/
-	public function extend($id, Closure $callable)
+	public function extend($id, $callable)
 	{
-		if (!array_key_exists($id, $this->values))
-		{
-			trigger_error(sprintf('Ключ "%s" не найден.', $id));
+        if (!isset($this->keys[$id])) {
+            throw new InvalidArgumentException(sprintf('Ключ «%s» не найден.', $id));
+        }
+
+        if (!is_object($this->values[$id]) || !method_exists($this->values[$id], '__invoke')) {
+            throw new InvalidArgumentException(sprintf('По ключу «%s» расположен не объект.', $id));
+        }
+
+        if (!is_object($callable) || !method_exists($callable, '__invoke')) {
+            throw new InvalidArgumentException('Расширение должно быть замыканием или объектом с определенным методом __invoke().');
+        }
+
+        $factory = $this->values[$id];
+
+        $extended = function ($c) use ($callable, $factory) {
+            return $callable($factory($c), $c);
+        };
+
+        if (isset($this->factories[$factory])) {
+            $this->factories->detach($factory);
+            $this->factories->attach($extended);
+        }
+
+        return $this[$id] = $extended;
+	}
+	
+	/**
+	* Задание фабричного сервиса
+	* При каждом вызове будет создаваться новый экземпляр
+	*/
+	public function factory($callable)
+	{
+		if (!is_object($callable) || !method_exists($callable, '__invoke')) {
+			throw new InvalidArgumentException('Необходимо передать замыкание или объект с определенным методом __invoke().');
 		}
 		
-		$factory = $this->values[$id];
+		$this->factories->attach($callable);
 		
-		if (!($factory instanceof Closure))
-		{
-			trigger_error(sprintf('Ключ "%s" не содержит объект.', $id));
-		}
-		
-		return $this->values[$id] = function($c) use ($callable, $factory)
-		{
-			return $callable($factory($c), $c);
-		};
+		return $callable;
 	}
 
 	/**
 	* Данный объект не будет вызван при обращении
 	* Его необходимо вызывать вручную
 	*/
-	public function protect(Closure $callable)
+	public function protect($callable)
 	{
-		return function($c) use ($callable)
-		{
-			return $callable;
-		};
+		if (!is_object($callable) || !method_exists($callable, '__invoke')) {
+			throw new InvalidArgumentException('Необходимо передать замыкание или объект с определенным методом __invoke().');
+		}
+		
+		$this->protected->attach($callable);
+		
+		return $callable;
 	}
 	
 	/**
-	* Извлечение параметра или определения объекта
+	* Извлечение параметра или замыкания, которое определяет объект
 	*/
 	public function raw($id)
 	{
-		if (!array_key_exists($id, $this->values))
-		{
-			trigger_error("Ключ «{$id}» не найден.");
+		if (!isset($this->keys[$id])) {
+			throw new InvalidArgumentException(sprintf('Ключ «%s» не найден.', $id));
+		}
+		
+		if (isset($this->raw[$id])) {
+			return $this->raw[$id];
 		}
 		
 		return $this->values[$id];
 	}
 
-	/**
-	* Объект-одиночка
-	*/
-	public function share(Closure $callable)
-	{
-		return function($c) use ($callable)
-		{
-			static $object;
-			
-			if (null === $object)
-			{
-				$object = $callable($c);
-			}
-			
-			return $object;
-		};
-	}
-	
 	public function load_constants($prefix)
 	{
-		if (!function_exists('apc_fetch'))
-		{
+		if (!function_exists('apc_fetch')) {
 			return false;
 		}
 
@@ -282,14 +298,12 @@ class application implements ArrayAccess
 
 	public function set_constants($prefix, $constants)
 	{
-		if (!function_exists('apc_fetch'))
-		{
-			foreach ($constants as $key => $value)
-			{
+		if (!function_exists('apc_fetch')) {
+			foreach ($constants as $key => $value) {
 				define($key, $value);
 			}
 		
-			return;
+			return true;
 		}
 	
 		apc_define_constants("{$prefix}_constants", $constants);
@@ -307,23 +321,48 @@ class application implements ArrayAccess
 	
 	public function offsetGet($id)
 	{
-		if (!array_key_exists($id, $this->values))
-		{
-			trigger_error(sprintf('Ключ "%s" не найден.', $id));
+		if (!isset($this->keys[$id])) {
+			throw new InvalidArgumentException(sprintf('Ключ «%s» не найден.', $id));
 		}
 		
-		$is_factory = is_object($this->values[$id]) && method_exists($this->values[$id], '__invoke');
+		if (isset($this->raw[$id])
+			|| !is_object($this->values[$id])
+			|| isset($this->protected[$this->values[$id]])
+			|| !method_exists($this->values[$id], '__invoke')
+		) {
+			return $this->values[$id];
+		}
 		
-		return $is_factory ? $this->values[$id]($this) : $this->values[$id];
+		if (isset($this->factories[$this->values[$id]])) {
+			return $this->values[$id]($this);
+		}
+		
+		$this->frozen[$id] = true;
+		$this->raw[$id] = $this->values[$id];
+		
+		return $this->values[$id] = $this->values[$id]($this);
 	}
 	
 	public function offsetSet($id, $value)
 	{
+		if (isset($this->frozen[$id])) {
+			throw new RuntimeException(sprintf('Нельзя изменять замороженный объект «%s»', $id));
+		}
+		
 		$this->values[$id] = $value;
+		$this->keys[$id] = true;
 	}
 	
 	public function offsetUnset($id)
 	{
 		unset($this->values[$id]);
+		
+		if (isset($this->keys[$id])) {
+			if (is_object($this->values[$id])) {
+				unset($this->factories[$this->values[$id]], $this->protected[$this->values[$id]]);
+			}
+			
+			unset($this->values[$id], $this->frozen[$id], $this->raw[$id], $this->keys[$id]);
+		}
 	}
 }
